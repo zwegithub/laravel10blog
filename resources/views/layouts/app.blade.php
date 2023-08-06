3cd9ff4c8c926364f392f4c00daafd1c777934e8
Leo Messi <zweyannaing.zwee@gmail.com>
2023-08-06 12:20:51 +0900

    edit again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ZWE YAN NAING - Blog</title>
+    <title>ZWE YAN NAING - Website </title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
